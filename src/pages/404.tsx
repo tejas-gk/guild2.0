6047b94b5352aeebedd44a8fd3dd9de5650985c9
tejas-gk <tejgankri@outlook.com>
2023-05-08 00:29:37 +0530

    chore:revamped 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import Link from 'next/link';
 
-export default function Custom404() {
-    return <div>404</div>;
-}
+const NotFoundPage = () => {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <h1 className='text-4xl font-bold text-gray-800 mb-4'>
+                Oops! Page not found.
+            </h1>
+            <p className='text-gray-600 mb-4'>
+                The page you were looking for doesn't exist.
+            </p>
+            <Link href='/'>
+                <p className='text-blue-500 hover:underline'>
+                    Go back to homepage
+                </p>
+            </Link>
+            {/* <img src='404.svg' alt="404 Error" className="w-96 mt-8" /> */}
+        </div>
+    );
+};
+
+export default NotFoundPage;

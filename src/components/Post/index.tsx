0063df424c74a2f3692c53d04647ab60df392f11
tejas-gk <tejgankri@outlook.com>
2023-05-25 19:40:16 +0530

    fix:posts

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import Avatar from './Avatar';
-import { PhotographIcon } from '@heroicons/react/outline';
+import { PhotographIcon, XIcon } from '@heroicons/react/outline';
 import { useRegisterModal } from '@/hooks/useRegisterModal';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import usePosts from '@/hooks/usePosts';
@@ -120,121 +120,160 @@ export default function Index({ postId, isComment = false }: PostProps): any {
                     </p>
                 </div>
             ) : (
-                <form
-                    className='
-                    rounded-md
-                    bg-white
-                    shadow-md
-                    border-gray-300
-                    p-2
-                    mx-7
-                    '
-                    onSubmit={onSubmit}
-                >
-                    <div
-                        className='
-                    flex
-                    items-center
-                    space-x-4
-                    justify-between
-                    '
-                    >
-                        <div>
-                            <Avatar seed={currentUser?.id} />
-                        </div>
-                        <div>
-                            {image && (
-                                <div className='flex justify-center'>
-                                    <Image
-                                        src={image}
-                                        alt='post'
-                                        className='w-1/2 h-1/2'
-                                    />
-                                </div>
-                            )}
-
-                            <input
-                                type='text'
-                                placeholder='Post something...'
-                                name='body'
-                                className='
-                                w-full h-12
-                                px-4 pl-5
-                                outline-none
-                                '
-                                onChange={(e) => {
-                                    setBody(e.target.value);
-                                    if (e.target.value === '') {
-                                        setIsTyping(false);
-                                    } else {
-                                        setIsTyping(true);
-                                    }
-                                }}
-                            />
-                        </div>
-
-                        <div className='relative cursor-pointer'>
-                            <input
-                                type='file'
-                                title='Upload Image'
-                                className='opacity-0 absolute inset-0 '
-                                onChange={handleFileChange}
-                            />
-                            {/* 
-                                todo: fix image upload
-                                */}
-                            {/* <ImageUpload  
-                                label='image'
-                                disabled={isLoading}
-                                onChange={handleFileChange}
-                                value={image}
-                            /> */}
-
-                            <label
-                                htmlFor='file-upload'
-                                className='flex items-center justify-center w-12 h-12 rounded-full cursor-pointer border border-gray-300'
-                            >
-                                <PhotographIcon className='h-6 w-6 text-gray-400 cursor-pointer' />
-                            </label>
-                        </div>
-                    </div>
-                    {isTyping && (
+                <div>
+                    {image && (
                         <div
-                            className='  
-                            '
+                            className='
+                            flex
+                             justify-center
+                             relative
+                             '
                         >
                             <div
                                 className='
-                                flex
-                                items-center
-                                px-2
-                                '
+                                absolute
+                                 top-0 right-0
+                                 '
                             >
-                                <p className='font-semibold'>Guild</p>
-                                <input
-                                    type='text'
-                                    placeholder='Select your Guild'
+                                <button
+                                    title='Remove image'
                                     className='
-                                        outline-none
-                                        bg-blue-50
-                                        flex-1
-                                        p-2
-                                        m-2
-                                        '
-                                />
+                                        bg-white
+                                         rounded-full 
+                                         p-1
+                                          shadow-md
+                                          '
+                                    onClick={() => setImage('')}
+                                >
+                                    <XIcon
+                                        className='h-6 w-6
+                                         text-gray-400
+                                          cursor-pointer
+                                          '
+                                    />
+                                </button>
+                            </div>
+
+                            <Image
+                                src={image}
+                                alt='post'
+                                height={200}
+                                width={400}
+                                className='w-full h-full
+                                
+                                    '
+                            />
+                        </div>
+                    )}
+
+                    <form
+                        className='
+                        rounded-md
+                         bg-white
+                          shadow-md
+                           border-gray-300
+                            p-2
+                            '
+                        onSubmit={onSubmit}
+                    >
+                        <div
+                            className='
+                        flex
+                         items-center
+                          space-x-4
+                          justify-between
+                          '
+                        >
+                            <div>
+                                <Avatar seed={currentUser?.id} />
                             </div>
                             <div
                                 className='
                             flex
-                            pr-5
-                            items-center
-                            '
+                             flex-col
+                              w-full
+                              '
                             >
-                                <Button title='Post' onClick={onSubmit} />
+                                <input
+                                    type='text'
+                                    placeholder='Post something...'
+                                    name='body'
+                                    className='w-full h-12
+                                     px-4 pl-5
+                                      outline-none
+                                      '
+                                    onChange={(e) => {
+                                        setBody(e.target.value);
+                                        setIsTyping(Boolean(e.target.value));
+                                    }}
+                                />
+                            </div>
+
+                            <div
+                                className='
+                            flex
+                            items-center
+                            relative'
+                            >
+                                <label
+                                    htmlFor='file-upload'
+                                    className="flex'
+                                       items-center
+                                       justify-center
+                                       w-12 h-12 
+                                       rounded-full
+                                       cursor-pointer
+                                       border
+                                      border-gray-300"
+                                >
+                                    <PhotographIcon
+                                        className='h-10 w-10
+                                     text-gray-400
+                                      cursor-pointer
+                                      m-1
+                                      '
+                                    />
+                                </label>
+                                <input
+                                    type='file'
+                                    title='Upload Image'
+                                    className='opacity-0 
+                                    absolute
+                                    inset-0'
+                                    onChange={handleFileChange}
+                                />
                             </div>
                         </div>
-                    )}
-                </form>
+                        {isTyping && (
+                            <div>
+                                <div
+                                    className='flex 
+                                items-center
+                                 px-2'
+                                >
+                                    <p className='font-semibold'>Guild</p>
+                                    <input
+                                        type='text'
+                                        placeholder='Select your Guild'
+                                        className='outline-none
+                                         bg-blue-50 
+                                         flex-1
+                                          p-2
+                                           m-2'
+                                    />
+                                </div>
+                                <div
+                                    className='flex 
+                                justify-end
+                                 pr-5
+                                  items-center'
+                                >
+                                    <Button title='Post' onClick={onSubmit} />
+                                </div>
+                            </div>
+                        )}
+                    </form>
+                </div>
             )}
         </>
     );
